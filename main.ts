efb338846a3ae1059ba8806d4a5c43b881f717de
Qwertvz <57400161+Qwertvz@users.noreply.github.com>
2023-09-29 12:55:37 +0000

    first

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { Express } from "express"
+const express = require("express")
+
+const app = express()
+app.get('/', function (req: any, res: any) {
+    res.send('Hello World')
+})
+
+app.listen(3000)
